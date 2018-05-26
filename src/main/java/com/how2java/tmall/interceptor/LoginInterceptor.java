c97d854b4d68df6d26cdcaf848ce8ac34cb58637
YangZhendong <404013419@qq.com>
2018-05-26 20:19:44 +0800

    购物车页面操作未完成

--- src/main/java/com/how2java/tmall/interceptor/LoginInterceptor.java
+++ src/main/java/com/how2java/tmall/interceptor/LoginInterceptor.java
@@ -1,9 +1,17 @@
 package com.how2java.tmall.interceptor;
 
+import com.how2java.tmall.pojo.User;
 import com.how2java.tmall.service.CategoryService;
 import com.how2java.tmall.service.OrderItemService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
     @Autowired
@@ -21,5 +29,54 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
      *    从最后一个拦截器往回执行所有的postHandle()
      *    接着再从最后一个拦截器往回执行所有的afterCompletion()
      */
-    
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        HttpSession session = request.getSession();
+        String contextPath = session.getServletContext().getContextPath();
+        String[] noNeedAuthPage = new String[]{
+                "home",
+                "checkLogin",
+                "register",
+                "loginAjax",
+                "login",
+                "product",
+                "category",
+                "search"};
+
+        String uri = request.getRequestURI();
+//        System.out.println("uri:" + uri);
+        uri = StringUtils.remove(uri, contextPath);
+//        System.out.println("contextPath:" + contextPath + "uri:" + uri);
+        if(uri.startsWith("/fore")){
+            String method = StringUtils.substringAfterLast(uri,"/fore" );
+            if(!Arrays.asList(noNeedAuthPage).contains(method)){
+                User user =(User) session.getAttribute("user");
+                if(null==user){
+                    response.sendRedirect("loginPage");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 在业务处理器处理请求执行完成后,生成视图之前执行的动作
+     * 可在modelAndView中加入数据，比如当前时间
+     */
+
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+    }
+
+    /**
+     * 在DispatcherServlet完全处理完请求后被调用,可用于清理资源等
+     *
+     * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()
+     */
+
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+    }
 }
